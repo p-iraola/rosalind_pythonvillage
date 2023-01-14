2db63e2303df4fcff6553e1be75c2c85d7a9d35d
p-iraola <102985671+p-iraola@users.noreply.github.com>
2023-01-14 12:08:30 -0500

    Update p5.py

--- p5.py
+++ p5.py
@@ -7,15 +7,16 @@ Words are case-sensitive, and the lines in the output can be in any order.
 
 string = 'When I find myself in times of trouble Mother Mary comes to me Speaking words of wisdom let it be And in my hour of darkness she is standing right in front of me Speaking words of wisdom let it be Let it be let it be let it be let it be Whisper words of wisdom let it be And when the broken hearted people living in the world agree There will be an answer let it be For though they may be parted there is still a chance that they will see There will be an answer let it be Let it be let it be let it be let it be There will be an answer let it be Let it be let it be let it be let it be Whisper words of wisdom let it be Let it be let it be let it be let it be Whisper words of wisdom let it be And when the night is cloudy there is still a light that shines on me Shine until tomorrow let it be I wake up to the sound of music Mother Mary comes to me Speaking words of wisdom let it be Let it be let it be let it be yeah let it be There will be an answer let it be Let it be let it be let it be yeah let it be Whisper words of wisdom let it be'
 
-words = string.split()
-count = dict()
 
-for word in words:
-	if word in count:
-		count[word] += 1
-	else:
-		count[word] = 1
+def num_occurences(string):
+  words = string.split()
+  count = dict()
 
-for key, value in count.items():
-	print(str(key) + ' ' + str(value))
-	
+  for word in words:
+    if word in count:
+      count[word] += 1
+    else:
+      count[word] = 1
+
+  for key, value in count.items():
+    print(f'{str(key)} {str(value)}')
