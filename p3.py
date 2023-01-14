f4f745426f7bb7e0906d8c6c5cf82cdd603481bb
p-iraola <102985671+p-iraola@users.noreply.github.com>
2023-01-14 12:01:04 -0500

    Update p3.py

--- p3.py
+++ p3.py
@@ -4,11 +4,11 @@ Given: Two positive integers a and b (a<b<10000).
 Return: The sum of all odd integers from a through b, inclusively.
 '''
 
-x = 0
-for i in range(4932,8978):
-	if i % 2 != 0:
-		x = i + x
+def sum_odd(a,b):
+  x = 0
+  for i in range(a,b+1):
+    if i % 2 != 0:
+      x += i
 
-print(x)
-
+  return(x)
 
